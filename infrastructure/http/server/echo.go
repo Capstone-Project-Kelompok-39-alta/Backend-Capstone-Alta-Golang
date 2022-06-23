b1c501e0e180fb9b662b47ff4cd67409dd9a19b4
sautmanurung1 <smanurung360@gmail.com>
2022-06-23 11:22:12 +0700

    success add swagger golang and update some error code

--- infrastructure/http/server/echo.go
+++ infrastructure/http/server/echo.go
@@ -14,7 +14,7 @@ import (
 // @title Automatic Generate Invoice System API Documentation
 // @description This is Automatic Generate Invoice API Documentation
 // @Version 2.0
-// @host localhost:8080
+// @host backend-capstone-alta-golang-staging.up.railway.app
 // @BasePath
 // @schemes http https
 // @securityDefinitions.apiKey JWT
