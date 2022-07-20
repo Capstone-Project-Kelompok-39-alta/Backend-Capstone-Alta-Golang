2b10657d578b55e6b5df5f078025ed91ca6dcd20
sautmanurung1 <smanurung360@gmail.com>
2022-07-20 17:08:11 +0700

    update about send email

--- service/admin/send_customer_service.go
+++ service/admin/send_customer_service.go
@@ -1,12 +1,10 @@
 package admin
 
 import (
-	"fmt"
 	domains "github.com/Capstone-Project-Kelompok-39-alta/Backend-Capstone-Alta-Golang/domains/admin"
 	"github.com/Capstone-Project-Kelompok-39-alta/Backend-Capstone-Alta-Golang/entities"
 	"github.com/Capstone-Project-Kelompok-39-alta/Backend-Capstone-Alta-Golang/infrastructure/database"
 	"github.com/Capstone-Project-Kelompok-39-alta/Backend-Capstone-Alta-Golang/lib"
-	"net/smtp"
 )
 
 type svcSendCustomer struct {
@@ -22,27 +20,9 @@ func NewSendCustomerService(repo domains.SendCustomerRepository, c database.Conf
 }
 
 func (s *svcSendCustomer) SendEmailService(message entities.SendCustomer) error {
-	server := "smtp-mail.outlook.com"
-	port := 587
-	user := "rolandbrilianto@outlook.com"
-	from := user
-	pass := "Kipasangin_1"
-	dest := message.To
-
-	auth := lib.LoginAuth(user, pass)
-
-	to := []string{dest}
-
-	messages := []byte("From: " + from + "\n" +
-		"To: " + dest + "\n" +
-		"Subject: " + message.Subject + "\n\n" +
-		message.Body)
-
-	endpoint := fmt.Sprintf("%v:%v", server, port)
-
-	err := smtp.SendMail(endpoint, auth, from, to, messages)
+	err := lib.SendEmail(message)
 	if err != nil {
-		fmt.Println("this is error guys :", err)
+		return err
 	}
 	return s.repo.SendEmail(message)
 }
